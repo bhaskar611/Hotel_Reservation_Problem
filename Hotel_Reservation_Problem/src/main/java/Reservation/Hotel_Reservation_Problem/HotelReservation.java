c017f422cc4c2823c6b314a902b40a75a165b17c
bhaskar611 <bhaskarnavuluri9@gmail.com>
2021-02-24 22:34:33 +0530

    Added: Welcome Message

---
+++ Hotel_Reservation_Problem/src/main/java/Reservation/Hotel_Reservation_Problem/HotelReservation.java
@@ -0,0 +1,9 @@
+package Reservation.Hotel_Reservation_Problem;
+
+public class HotelReservation {
+
+	public static void main(String[] args) {
+		
+		System.out.println("welcome to hotel Reservation Problem");
+}
+}
