d0c520f768d3b9409c9bd9b60366bf14c099cbf2
bhaskar611 <bhaskarnavuluri9@gmail.com>
2021-02-27 17:17:31 +0530

    Added:best rated hotel for given range

--- Hotel_Reservation_Problem/src/main/java/Reservation/Hotel_Reservation_Problem/HotelReservation.java
+++ Hotel_Reservation_Problem/src/main/java/Reservation/Hotel_Reservation_Problem/HotelReservation.java
@@ -6,8 +6,7 @@ import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 
 public class HotelReservation {
-
-	ArrayList<Hotel> hotelList = new ArrayList<>();
+	ArrayList<Hotel> hotelList;
 	long minCostLakeWood;
 	long minCostBridgeWood;
 	long minCostRidgeWood;
@@ -15,38 +14,38 @@ public class HotelReservation {
 	long weekDays;
 	long weekendDays;
 	int rating;
-
 	
-	public void addHotelDetails() {
-		hotelList.add(new Hotel("Lakewood", 3, 110, 90));
-		hotelList.add(new Hotel("Bridgewood", 4, 150, 50));
-		hotelList.add(new Hotel("Ridgewood", 5, 220, 150));
+	// Constructor
+		public HotelReservation() {
+		hotelList = new ArrayList<>();
+	}
+	
+	
+
+	public void HotelDetails() {
+		hotelList.add(new Hotel("LakeWood", 3, 110, 90));
+		hotelList.add(new Hotel("BridgeWood", 4, 150, 50));
+		hotelList.add(new Hotel("RidgeWood", 5, 220, 150));
 	}
 
-	private static long countWeekDaysMath(LocalDate arrivalDay, LocalDate departureDay) {
+	// count week days 
+	private static long countWeekDaysMath(LocalDate arrival, LocalDate departure) {
 		long count = 0;
-		final DayOfWeek startDay = arrivalDay.getDayOfWeek();
-		final DayOfWeek endDay = departureDay.getDayOfWeek();
+		final DayOfWeek startDay = arrival.getDayOfWeek();
+		final DayOfWeek endDay = departure.getDayOfWeek();
 
-		final long days = ChronoUnit.DAYS.between(arrivalDay, departureDay);
+		final long days = ChronoUnit.DAYS.between(arrival, departure);
 		final long daysWithoutWeekends = days - 2 * ((days + startDay.getValue()) / 7);
 
-	
+		// adjust for starting and ending on a Sunday:
 		count = daysWithoutWeekends + (startDay == DayOfWeek.SUNDAY ? 1 : 0) + (endDay == DayOfWeek.SUNDAY ? 1 : 0);
 		return count;
 	}
-	
-	public void findMinimumCostHotel() {
-		
-	}
 
-
-	public ArrayList<String> findCheapestCost(long weekDays, long weekendDays) {
+	// Find cheapest hotel
+	public ArrayList<String> CheapestCost(long weekDays, long weekendDays) {
 		ArrayList<String> hotel = new ArrayList<>();
-		minCostLakeWood = 0;
-		minCostBridgeWood = 0;
-		minCostRidgeWood = 0;
-		rating = 0;
+		minCostLakeWood = 0;long cost = 0; minCostBridgeWood = 0; minCostRidgeWood = 0; rating = 0; String hotelName = null;
 		minCostLakeWood = (hotelList.get(0).getRegularRate() * weekDays
 				+ hotelList.get(0).getWeekendRate() * weekendDays);
 		minCostBridgeWood = (hotelList.get(1).getRegularRate() * weekDays
@@ -54,34 +53,37 @@ public class HotelReservation {
 		minCostRidgeWood = (hotelList.get(2).getRegularRate() * weekDays
 				+ hotelList.get(2).getWeekendRate() * weekendDays);
 		long minCostHotel = Math.min(minCostRidgeWood, Math.min(minCostBridgeWood, minCostLakeWood));
-		if (minCostHotel == minCostRidgeWood && (hotelList.get(2).getHotelRating() > rating)) {
+		if ((hotelList.get(2).getHotelRating() > rating)) {
+			hotelName = hotelList.get(2).getHotelName();
+			cost = minCostRidgeWood;
 			rating = hotelList.get(2).getHotelRating();
-			hotel.add("RidgeWood");
+			hotel.add(hotelName);
 		}
-		if (minCostHotel == minCostBridgeWood && (hotelList.get(1).getHotelRating() > rating)) {
+		if ((hotelList.get(1).getHotelRating() > rating)) {
+			hotelName = hotelList.get(1).getHotelName();
+			cost = minCostBridgeWood;
 			rating = hotelList.get(1).getHotelRating();
-			hotel.add("BridgeWood");
+			hotel.add(hotelName);
 		}
-		if (minCostHotel == minCostLakeWood && (hotelList.get(0).getHotelRating() > rating)) {
+		if ((hotelList.get(0).getHotelRating() > rating)) {
+			hotelName = hotelList.get(0).getHotelName();
+			cost = minCostLakeWood;
 			rating = hotelList.get(0).getHotelRating();
-			hotel.add("LakeWood");
+			hotel.add(hotelName);
 		}
-		System.out.println("The minimum cost for Hotel is " + minCostHotel + " and rating of hotel is " + rating);
+		System.out.println("Cost for "+hotelName+" is " + cost + " & rating is " + rating);
 		return hotel;
 	}
 
-
-	public ArrayList<String> CheapestHotel(String dateOfArrival, String dateOfDeparture)
-			throws Exception {
-		totalDays = 0;
-		weekDays = 0;
-		weekendDays = 0;
+	// Calculated rate based on period of stay at hotel
+	public ArrayList<String> CheapestHotel(String dateOfArrival, String dateOfDeparture)throws Exception {
+		totalDays = 0; weekDays = 0; weekendDays = 0;
 		LocalDate dateArrival = LocalDate.parse(dateOfArrival);
 		LocalDate dateDeparture = LocalDate.parse(dateOfDeparture);
 		totalDays = ChronoUnit.DAYS.between(dateArrival, dateDeparture) + 1;
 		weekDays = countWeekDaysMath(dateArrival, dateDeparture);
 		weekendDays = totalDays - weekDays;
-		return findCheapestCost(weekDays, weekendDays);
+		return CheapestCost(weekDays, weekendDays);
 	}
 
 }
--- Hotel_Reservation_Problem/src/test/java/Reservation/Hotel_Reservation_Problem/HotelReservationTest.java
+++ Hotel_Reservation_Problem/src/test/java/Reservation/Hotel_Reservation_Problem/HotelReservationTest.java
@@ -3,7 +3,6 @@ package Reservation.Hotel_Reservation_Problem;
 import java.util.ArrayList;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -18,30 +17,30 @@ public class HotelReservationTest {
 	
 
 	@Test
-	public void givenDate_WeekDayshouldReturn_CheapestHotelNameBridgeWood() throws Exception {
-		hotelReservation.addHotelDetails();
+	public void given_WeekDay_Return_CheapestHotelNameRidgeWood() throws Exception {
+		hotelReservation.HotelDetails();
 		ArrayList<String> hotelNameList = hotelReservation.CheapestHotel("2020-09-14", "2020-09-16");
 		Object[] hotelName = hotelNameList.toArray();
-		Object[] arrayExpectedOutput = { "LakeWood" };
-		Assert.assertArrayEquals(arrayExpectedOutput, hotelName);
+		Object[] ExpectedOutput = { "RidgeWood" };
+		Assert.assertArrayEquals(ExpectedOutput, hotelName);
 	}
 
 	@Test
-	public void givenDateWeekend_shouldReturn_CheapestHotelNameBrideWood() throws Exception {
-		hotelReservation.addHotelDetails();
+	public void given_Weekend_Return_CheapestHotelNameRideWood() throws Exception {
+		hotelReservation.HotelDetails();
 		ArrayList<String> hotelNameList = hotelReservation.CheapestHotel("2020-09-12", "2020-09-13");
 		Object[] hotelName = hotelNameList.toArray();
-		Object[] arrayExpectedOutput = { "BridgeWood" };
-		Assert.assertArrayEquals(arrayExpectedOutput, hotelName);
+		Object[] ExpectedOutput = { "RidgeWood" };
+		Assert.assertArrayEquals(ExpectedOutput, hotelName);
 	}
 
 	@Test
-	public void givenDateWeekDayWeekend_shouldReturn_CheapestHotelNameGivesBridgeWood() throws Exception {
-		hotelReservation.addHotelDetails();
+	public void givenWeekDayWeekend_Return_CheapestHotelNameGivesRidgeWood() throws Exception {
+		hotelReservation.HotelDetails();
 		ArrayList<String> hotelNameList = hotelReservation.CheapestHotel("2020-09-11", "2020-09-12");
 		Object[] hotelName = hotelNameList.toArray();
-		Object[] arrayExpectedOutput = { "BridgeWood" };
-		Assert.assertArrayEquals(arrayExpectedOutput, hotelName);
+		Object[] ExpectedOutput = { "RidgeWood" };
+		Assert.assertArrayEquals(ExpectedOutput, hotelName);
 	}
 
 }
